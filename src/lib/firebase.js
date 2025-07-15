e7af6c8a3bd1c815f4cfb0a4bd68e0c4c149152d
n4nd4n <nikhilnandan0207@gmail.com>
2025-07-15 19:59:40 +0530

    🔥 Fixed Firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyCxP_RJrujOWXaqTxPoSUjdXLas0kqgzXc",
+  authDomain: "ccl-employee-management.firebaseapp.com",
+  projectId: "ccl-employee-management",
+  storageBucket: "ccl-employee-management.appspot.com", // fixed this line
+  messagingSenderId: "338659344810",
+  appId: "1:338659344810:web:cf18de3de5214045e7ebd3",
+  measurementId: "G-0JT8EPK996" // optional
 };
 
 const app = initializeApp(firebaseConfig);
